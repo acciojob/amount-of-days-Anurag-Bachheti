f03ecad6e599fe0fb9636049da1a23b88780c975
Anurag Bachheti <157354714+Anurag-Bachheti@users.noreply.github.com>
2025-03-18 19:50:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function daysOfYear(year) {
 	if(year<1 && year>9999)
 		return("Invalid Year");
 	}
-	return(year%4===0 && year%100!==0) || (year%400===0) ? 366:365;
+	return(year%4===0 && year%100!==0) && (year%400===0) ? 366 : 365;
 }
 
 console.log(daysOfYear(2022));
